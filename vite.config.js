39fdb27a0bcd4774681f3f78b3cd8510e0bb3782
Datla Vasantha Varma <112185428+vasanthvarmadatla@users.noreply.github.com>
2025-06-14 11:02:53 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Calendar_/', // 👈 important
   plugins: [react()],
+  base: './', // <- THIS FIXES BLANK PAGE ON HOSTING
 })
